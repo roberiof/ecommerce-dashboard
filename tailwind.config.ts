01bbbde3220495887f6be2de40b4503026ad5964
Robério Filho <107323497+roberiof@users.noreply.github.com>
2024-12-21 12:44:49 -0300

    (feat): initialize project (next, shadcn ui, eslint)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "nunito-sans": ["var(--font-nunito-sans)", ...fontFamily.sans]
+      },
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
-    },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")]
 } satisfies Config;
